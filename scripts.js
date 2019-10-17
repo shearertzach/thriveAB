4480b9a0c6f3d0bce29c34aaa0b2e78f72b40b3e
Justin Browning <justinbrowning@iMac-for-Printing-JBrow.local>
2019-10-17 14:38:14 -0500

    Modified the front page. Started to design a dashboard

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function closeNav() {
+    var sidebar = document.getElementById("sidebar")
+
+    sidebar.style.width = "0px"
+
+}
+
+function openNav() {
+    var sidebar = document.getElementById("sidebar")
+
+    sidebar.style.width = "250px"
+
+}
