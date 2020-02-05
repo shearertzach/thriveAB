ad2d46d61640fe031c9ea82b31c52b899cd9177a
Zachary Harper <zachary.harper402@gmail.com>
2020-02-05 08:36:29 -0600

    create post modal testOne

--- src/features/Discussion/components/CreatePost/index.js
+++ src/features/Discussion/components/CreatePost/index.js
@@ -3,6 +3,25 @@ import { connect } from 'react-redux'
 import Input from '../../../../shared/components/Input'
 import Button from '../../../../shared/components/Button'
 import { submitDiscussionPost } from '../../redux/discussionActions';
+import "../CreatePost/createPostModal";
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
 
 class CreatePost extends PureComponent {
     
@@ -45,14 +64,23 @@ class CreatePost extends PureComponent {
             }
             else { console.log('User can not post.') }
         }
+    
+    
         
     render() {
-        return(
+        return (
             <div>
+            <button class="trigger">Click here to trigger the modal!</button>
+    <div class="modal">
+                <div class="modal-content">
                 <Input id='Post-Title' onChange={this.handleTitle} />
                 <Input id= 'Post-Text' onChange={this.handleText} />
-                <Button onClick={this.handleSubmit} /* Needs button text */ />
-            </div>
+           <Button onClick={this.handleSubmit} class="close-button" /* Needs button text */ > Submit Post! </Button>
+          
+        </div>
+                </div>
+                </div>
+            
         )
     }
 
